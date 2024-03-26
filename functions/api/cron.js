66f109ab6c88a82c40aad2b88e1eabfeeaaad100
Konrad <konrad.kalicinski@external.nexigroup.com>
2024-03-26 14:28:56 +0100

    cleanup

--- functions/api/cron.js
+++ functions/api/cron.js
@@ -3,18 +3,15 @@ import {CurrencySerice} from "../services/currency.service";
 export default async function handler(req, res) {
   try {
     const apiUrl = `https://api.currencyapi.com/v3/latest?apikey=${process.env.CURRENCY_API_KEY}`
-    const response = await fetch(apiUrl);
-    const currencyToInsert = await response.json();
+    const currencyApiResponse = await fetch(apiUrl);
+    const currencyToInsert = await currencyApiResponse.json();
 
-    const previousCurrency = await CurrencySerice.getLatestCurrency();
+    const lastInsertedCurrency = await CurrencySerice.getLatestCurrency();
 
-    const prevDay = previousCurrency?.time.split('T')[0];
+    const lastInsertedDay = lastInsertedCurrency?.time.split('T')[0];
     const today = (new Date()).toISOString().split('T')[0];
 
-
-    console.log({prevDay, today});
-
-    if (today !== prevDay) {
+    if (today !== lastInsertedDay) {
       await CurrencySerice.insertCurrency(currencyToInsert);
       console.log('Inserted new currency');
       res.status(200).json({message: 'Inserted new currency'});
