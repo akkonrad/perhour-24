df2c15463dc4294e7d2f68da76f0fea4112e3da8
Konrad <konrad.kalicinski@external.nexigroup.com>
2024-03-26 14:02:59 +0100

    adding api functions

---
+++ functions/api/currency.js
@@ -0,0 +1,15 @@
+import { CurrencySerice } from '../services/currency.service';
+export default function handler(req, res) {
+  async function run() {
+    try {
+      const data =  await CurrencySerice.getLatestCurrency();
+      res.status(200).json(data);
+    }
+    catch (error) {
+      console.error(error);
+      res.status(500).json({error: error.message});
+    }
+  }
+  run().catch(console.dir);
+
+}
